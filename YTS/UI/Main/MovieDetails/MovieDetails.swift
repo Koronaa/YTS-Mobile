545ec1aa2d0dab027311794e07956c8cd2d9cb0a
Sajith Konara <konara.sajith@gmail.com>
2020-04-15 18:36:07 +0530

    [POD] Cosmos

---
+++ YTS/UI/Main/MovieDetails/MovieDetails.swift
@@ -0,0 +1,29 @@
+//
+//  MovieDetails.swift
+//  YTS
+//
+//  Created by Sajith Konara on 4/15/20.
+//  Copyright © 2020 Sajith Konara. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailsView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    
+    private func commonInit(){
+        Bundle.main.loadNibNamed("", owner: self, options: nil)
+        
+    }
+
+}
